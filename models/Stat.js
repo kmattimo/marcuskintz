b9fcecebae676c50cf3307f0bf8b10638831e134
kmattimo <kmattimo@umich.edu>
2015-08-02 23:47:48 -0500

    regenerated with sass

---
+++ models/Stat.js
@@ -0,0 +1,20 @@
+var keystone = require('keystone');
+
+/**
+ * Stat Model
+ * ==================
+ */
+
+var Stat = new keystone.List('Stat', {
+	autokey: { from: 'name', path: 'key', unique: true },
+	sortable: true
+});
+
+Stat.add({
+	name: { type: String, required: true, initial: true },
+	value: {type: String, initial:true },
+	hidden: {type: Boolean, default:false}
+});
+
+Stat.defaultColums ='name, value'
+Stat.register();
