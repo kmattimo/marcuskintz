4be3f5c7bd372fd17c6c184616a80899c1ef97dd
Kyle Mattimore <kmattimo@umich.edu>
2015-08-28 20:55:54 -0500

    profile model

---
+++ models/Profile.js
@@ -0,0 +1,23 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+/**
+ * Profile Model
+ */
+
+var Profile = new keystone.List('Profile', {
+	nocreate: true,
+	nodelete: true,
+	autokey: { from: 'name', path: 'key', unique: true },
+	sortable: true
+});
+
+Profile.add({
+	SiteTitle: { type: String, default:"Marcus Kintz- Athletic Profile" },
+	SiteHeader: { type: String, default:"Marcus Kintz- Athletic Profile" },
+	Statement: { type: Types.Textarea }
+});
+
+Profile.register();
+
+
+
